7171c35d03ddfdda7bd79b5b82580ee4acf44990
Ahmed Zaghloul <ahmed@Ahmeds-MacBook-Air.local>
2022-08-30 20:54:28 +0200

    Third commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Lab 5
 // 
+var id=1;
 
 // Task 0 
 class PersonClass{
@@ -45,7 +46,8 @@ class employee extends PersonClass{
   constructor(name,money,salary,email,isManager) {
     super(name,money);
     this.salary=salary;
-    this.id=email;
+    this.id=i;
+    i+=1;
     this.email=email;
     this.workMood;
     this.isManager=isManager;
@@ -75,18 +77,72 @@ class office{
 
   getAllEmployees(){
     for(let i=0;i<this.employees.length;i++){
-      console.log("Name: "+this.employees[i].name+"\nId and Email: ")
-    }
-  }
-
-  getEmployee(id){
-    for(let i=0;i<this.employees.length;i++){
+      document.write("<h2>All Employees</h2>")
       if(this.employees[i].isManager === true){
-        console.log();
+        // console.log("Name: "+this.employees[i].name+"\nId and Email: "+ this.employees[i])
+        document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
+        "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Title: Manager</p>");
+      }else{
+        document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
+        "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Salary: </p>"+ this.employees[i].salary);
       }
     }
   }
 
+  getEmployee(idEmail){
+    for(let i=0;i<this.employees.length;i++){
+      if(this.employees[i].id == idEmail || this.employees[i].email ==idEmail){
+        if(this.employees[i].isManager === true){
+          // console.log("Name: "+this.employees[i].name+"\nId and Email: "+ this.employees[i])
+          document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
+          "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Title: Manager</p>");
+          break;
+        }else{
+          document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
+          "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Salary: </p>"+ this.employees[i].salary);
+          break;
+        }
+      }
+    }
+  }
+
+  hire(emp){
+    this.employees.push(emp);
+  }
+
+
+  
+  /*fire(empId){
+    for(let i=0;i<this.employees.length;i++){
+      if(this.employees[i].id == empId){
+        if(i==0){
+          this.employees.shift();
+          break;
+        }else if(i == (this.employees.length-1)){
+          this.employees.pop();
+          break;
+        }else{
+          let tempEmployee = this.employees.filter(checkId)
+
+          checkId(emplID){
+            return emplID != empId;
+          }
+
+        }
+      }
+    }
+  }*/
+
+  fire(empId){
+
+    let tempEmployee = this.employees.filter(checkId);
+
+    this.employees = tempEmployee;
+
+    function checkId(emplID){
+      return emplID != empId;
+    }
+  }
 }
 ////////// Calling Tasks Funtions //////////
 
