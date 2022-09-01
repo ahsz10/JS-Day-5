d95541c576e92cd44c9078ebc128d9abaccbd208
Ahmed Zaghloul <ahmed@Ahmeds-MacBook-Air.local>
2022-09-01 05:51:20 +0200

    commit 10

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Lab 5
-// 
 var id=1;
 
 // Task 0 
@@ -47,8 +46,8 @@ class employee extends PersonClass{
   constructor(name,age,salary,email,isManager) {
     super(name,age);
     this.salary=salary;
-    this.id=i;
-    i+=1;
+    this.id=id;
+    id+=1;
     this.email=email;
     this.workMood;
     this.isManager=isManager;
@@ -77,34 +76,37 @@ class office{
   }
 
   getAllEmployees(){
+    document.write("<h2>All Employees</h2>")
     for(let i=0;i<this.employees.length;i++){
-      document.write("<h2>All Employees</h2>")
       if(this.employees[i].isManager === true){
         // console.log("Name: "+this.employees[i].name+"\nId and Email: "+ this.employees[i])
-        document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
-        "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Title: Manager</p>");
+        document.write("<p>Name: </p>"+this.employees[i].name+"<p>Id: </p>"+ this.employees[i].id+
+        "<p>Id and Email: </p>"+ this.employees[i].email+"<p>Title: Manager</p> <br><hr>");
       }else{
-        document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
-        "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Salary: </p>"+ this.employees[i].salary);
+        document.write("<p>Name: </p>"+this.employees[i].name+"<p>Id: </p>"+ this.employees[i].id+
+        "<p>Id and Email: </p>"+ this.employees[i].email+"<p>Salary: </p>"+ this.employees[i].salary+"<hr>");
       }
     }
+    document.write("<hr><hr><hr>");
   }
 
   getEmployee(idEmail){
+    document.write("<h2>Searching for emplyee with id: </h2>"+idEmail)
     for(let i=0;i<this.employees.length;i++){
       if(this.employees[i].id == idEmail || this.employees[i].email ==idEmail){
         if(this.employees[i].isManager === true){
           // console.log("Name: "+this.employees[i].name+"\nId and Email: "+ this.employees[i])
           document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
-          "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Title: Manager</p>");
+          "<p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Title: Manager</p>");
           break;
         }else{
-          document.write("<p>Name: </p>"+this.employees[i].name+"<br> <p>Id: </p>"+ this.employees[i].id+
-          "<br> <p>Id and Email: </p>"+ this.employees[i].email+"<br> <p>Salary: </p>"+ this.employees[i].salary);
+          document.write("<p>Name: </p>"+this.employees[i].name+"<p>Id: </p>"+ this.employees[i].id+
+          "<p>Id and Email: </p>"+ this.employees[i].email+"<p>Salary: </p>"+ this.employees[i].salary);
           break;
         }
       }
     }
+    document.write("<hr>");
   }
 
   hire(emp){
@@ -125,16 +127,28 @@ class office{
 
 
 function createEmployee(){
-    const name = (window.prompt("Enter Name: ")).toLowerCase;
-    const age = (window.prompt("Enter Age: ")).toLowerCase;
-    const salary = (window.prompt("Enter Salary: ")).toLowerCase;
-    const email = (window.prompt("Enter email: ")).toLowerCase;
-    const manager = (window.prompt("Is he a manager:\n (Yes/No) ")).toLowerCase; 
-    let manFlag = false;
-    if(manager === "yes"|| manager === "y" || manager.includes("yes")){
-      manFlag = true;
+    const name = window.prompt("Enter Name: ");
+    const nameLower= name.toLowerCase();
+    const age = window.prompt("Enter Age: ");
+    let salary;
+    while(true){
+      salary = parseInt(window.prompt("Enter Salary: (Have to be greater than 1000) ")); 
+      if(salary>1000){
+        break;
+      }
     }
-    let temp = new employee(name,age,salary,email,manFlag);
+    const email = window.prompt("Enter email: ");
+    const emailLower= email.toLowerCase();
+    const manager = window.prompt("Is he a manager:\n (Yes/No) "); 
+    const managerLower =manager.toLowerCase();
+    let manFlag;
+    if(managerLower === "yes"|| managerLower === "y" ){
+      manFlag = true;
+    }else{
+      manFlag = false;
+    }
+    let temp = new employee(nameLower,age,salary,emailLower,manFlag);
+    console.log(temp);
 
     return temp;
 }
@@ -151,8 +165,7 @@ function searchEmployee(office,id){
 }
 
 function startProgram(){
-  let mainOffice = new office(newOffice1);
-
+  var mainOffice = new office("newOffice1");
   while(true){
     let usrInput= window.prompt("Enter the function you want to do: "+
     "\nAdding a new employee(hire an employee), fire an employee, search for an employee and list all Employees details"+
@@ -160,10 +173,9 @@ function startProgram(){
     let usrInputLower = usrInput.toLowerCase();
 
     if(usrInputLower.includes("hire") || usrInputLower === "hire" || usrInputLower.includes("add") || usrInputLower === "add"){
-      // alert("Please make sure that you added the before choosing Hire")
       mainOffice.hire(createEmployee());
     }else if(usrInputLower.includes("fire") || usrInputLower === "fire"){
-      let fireId = window.prompt("Firing an Employee \nEnter Email or ID: ");
+      let fireId = window.prompt("Firing an Employee \nEnter Employee ID: ");
       if(searchEmployee(mainOffice,fireId)){
         mainOffice.fire(fireId);
         alert("The employee with id: "+fireId+" is fired")
@@ -171,7 +183,12 @@ function startProgram(){
         alert("Invalid ID. Please try again")
       }
     }else if(usrInputLower.includes("search") || usrInputLower === "search" ){
-      mainOffice.getEmployee((window.prompt("Searching for an Employee \nEnter Email or ID: ")).toLowerCase);
+      let searchId =parseInt(window.prompt("Searching for an Employee \nEnter ID: "));
+      if(searchEmployee(mainOffice,searchId)){
+        mainOffice.getEmployee(searchId);
+      }else{
+        alert("Invalid ID. Please try again")
+      }
     }else if(usrInputLower.includes("list") || usrInputLower.includes("list all") || usrInputLower === "list all"){
       mainOffice.getAllEmployees();
     }else if(usrInputLower === "quit" || usrInputLower === "q"){
@@ -187,7 +204,4 @@ function startProgram(){
 ////////// Calling Tasks Funtions //////////
 
 startProgram();
-
-
-
 
